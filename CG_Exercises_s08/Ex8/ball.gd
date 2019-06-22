9ac94d653f0fe13b8c083a09decc441cf927abec
Oliver Mautschke <olivermautschke@gmail.com>
2019-06-22 21:25:40 +0200

    EasyMode

--- CG_Exercises_s08/Ex8/ball.gd
+++ CG_Exercises_s08/Ex8/ball.gd
@@ -14,6 +14,9 @@ var derivatives = [ Vector2(1, 0),
 var speed = 100
 var mode = 0
 
+var prev = 0
+var next = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.waypoints.append(self.get_parent().get_node("sphere1"))
@@ -39,40 +42,13 @@ func move(time):
 	var h01 = pow(time, 3) -2 * pow(time, 2) + time
 	var h11 = pow(time, 3) - pow(time, 2)
 	
-	var p0 = Vector2(0, 0)
-	var p1 = Vector2(0, 0)
-	var d0 = Vector2(0, 0)
-	var d1 = Vector2(0, 0)
+	var p0 = self.waypoints[self.prev].position
+	var p1 = self.waypoints[self.next].position
+	var d0 = self.derivatives[self.prev]
+	var d1 = self.derivatives[self.next]
 	
-	for i in range(self.waypoints.size()):
-		var next = 0
-		var prev = 0
-		if i == 0:
-			next = i + 1
-			#prev = self.waypoints.size() - 1
-			prev = 0
-		elif i == (self.waypoints.size() - 1):
-			next = self.waypoints.size() - 1
-			prev = i - 1
-		else:
-			next = i + 1
-			prev = i - 1
-		
-		if (self.position.distance_to(self.waypoints[i].position)) < self.waypoints[prev].position.distance_to(self.waypoints[next].position):
-			p0 = self.waypoints[i].position
-
-			p1 = self.waypoints[next].position
-			
-			if self.mode == 0:
-				d0 = self.derivatives[i] * self.speed
-				d1 = self.derivatives[next] * self.speed
-			elif self.mode == 1:
-				var Tk = 0.5
-				d0 = Tk * (p1 - p0)
-				d1 = Tk * (p1 - p0)
-				
-			self.position = h00 * p0 + h10 * p1 + h01 * d0 + h11 * d1
-			break
-
-	if self.position == self.waypoints[4].position:
-		self.position = Vector2(0, 250)
+	self.position = h00 * p0 + h10 * p1 + h01 * d0 + h11 * d1
+	
+	print(h00 * p0 + h10 * p1 + h01 * d0 + h11 * d1)
+	
+	
